ab22a6e4315e046319fba640c4db51652da4fb71
Dionysis Zindros <dionyziz@gmail.com>
2012-02-06 07:47:49 +0000

    Problem 119 solved

--- problem-119/119.cpp
+++ problem-119/119.cpp
@@ -1,31 +1,46 @@
 #include <cstdio>
 #include <cmath>
+#include <set>
 
 typedef unsigned long long int LL;
-typedef long double LLD;
 
-const LLD EPSILON = 0.00001;
+using namespace std;
+
+inline LL sigma( LL i ) {
+    LL sum = 0;
+    LL base = 1;
+    LL t = i;
+
+    while ( t ) {
+        sum += t % 10;
+        t /= 10;
+        base *= 10;
+    }
+    return sum;
+}
+
+void printSolutions( const set< LL > solutions ) {
+    int i = 1;
+
+    for ( set< LL >::iterator it = solutions.begin(); it != solutions.end(); ++it, ++i ) {
+        printf( "a[ %i ] = %lli\n", i, *it );
+    }
+}
 
 int main() {
-    LLD l;
-    LL i, t, base, sum;
-    int j, prev = 0;
+    set< LL > solutions;
 
-    for ( i = 10, j = 1; j <= 100; ++i ) {
-        sum = 0;
-        base = 1;
-        t = i;
-        while ( t ) {
-            sum += t % 10;
-            t /= 10;
-            base *= 10;
-        }
-        if ( sum > 1 ) {
-            l = log( ( LLD )i ) / log( ( LLD )sum );
-            if ( fabs( l - ( LL )l ) < EPSILON && pow( sum, ( LL )l ) == i ) {
-                // whole number
-                printf( "a[ %i ] = %lli\n", j, i );
-                ++j;
+    for ( LL l = 1; ; ++l ) { // dove-tailing
+        for ( LL s = 1; s < l; ++s ) {
+            LL e = l - s;
+            LL i = pow( s, e );
+
+            if ( i > 10 ) {
+                if ( sigma( i ) == s ) {
+                    solutions.insert( i );
+                    printf( "Found a new solution.\n" );
+                    printSolutions( solutions );
+                }
             }
         }
     }
