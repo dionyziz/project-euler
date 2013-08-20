274913f499804f4ec6d1fd58a2569cdfccbbdb7f
Dionysis Zindros <dionyziz@gmail.com>
2013-08-21 00:32:14 +0300

    Code improvements for 189 - generalize in K

--- problem-189/189.py
+++ problem-189/189.py
@@ -1,31 +1,29 @@
 import itertools
 
-DP = [None, {(0): 1, (1): 1, (2): 1}]
-
+K = 3
 N = 8
+
+colors = range(0, K)
+DP = [None, {(i,): 1 for i in colors}]
 
 def flip(a):
     number = []
     for i in a:
-        digit = [d for d in (0, 1, 2) if d != i]
+        digit = [d for d in colors if d != i]
         number.append(digit)
     return itertools.product(*number)
 
 def double_flip(a):
-    number = [[d for d in (0, 1, 2) if d != a[0]]]
+    number = [[d for d in colors if d != a[0]]]
     for i in range(0, len(a) - 1):
-        number.append([d for d in (0, 1, 2) if d != a[i] and d != a[i + 1]])
-    number.append([d for d in (0, 1, 2) if d != a[len(a) - 1]])
+        number.append([d for d in colors if d != a[i] and d != a[i + 1]])
+    number.append([d for d in colors if d != a[len(a) - 1]])
     return itertools.product(*number)
 
 for i in range(2, N + 1):
     DP.append({})
     for a, v in DP[i - 1].iteritems():
-        if isinstance(a, tuple):
-            la = list(a)
-        else:
-            la = [a]
-        for a_prime in flip(la):
+        for a_prime in flip(list(a)):
             for b in double_flip(a_prime):
                 bt = tuple(b)
                 if bt not in DP[i]:
