ed5f7d2d4a43c79755326e2ee54bf6c1852e946d
Dionysis Zindros <dionyziz@gmail.com>
2014-04-24 03:42:37 +0300

    Solve 81 in haskell

---
+++ problem-81/81.hs
@@ -0,0 +1,12 @@
+import Data.List.Split
+
+foldRow :: [Int] -> [Int] -> [Int]
+foldRow (d:ds) (e:es) = reverse $ foldl foldCol [e + d] $ zip ds es
+
+foldCol :: [Int] -> (Int, Int) -> [Int]
+foldCol (a:as) (dp, elem) = (min a dp) + elem:a:as
+
+main = do
+    contents <- getContents
+    let input = map (map read) $ map (splitOn ",") $ lines contents :: [[Int]]
+    print $ last $ foldl foldRow (scanl1 (+) $ head input) $ tail input
