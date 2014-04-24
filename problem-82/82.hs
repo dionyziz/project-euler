46ee3d97de003d5d19a1b2c6c23240e28d731e0b
Dionysis Zindros <dionyziz@gmail.com>
2014-04-24 13:16:28 +0300

    Simplify code for 82

--- problem-82/82.hs
+++ problem-82/82.hs
@@ -2,18 +2,17 @@ import Data.List
 import Data.List.Split
 
 foldCol :: [Int] -> [Int] -> [Int]
-foldCol dp col = reverse $ foldl (walk dp col) [] $ zip col [0..]
-
-walk :: [Int] -> [Int] -> [Int] -> (Int, Int) -> [Int]
-walk dp col acc target@(end, i) = (minimum $ map (path col target) $ zip dp [0..]):acc
-
-path :: [Int] -> (Int, Int) -> (Int, Int) -> Int
-path col (end, i) (start, j) = start + (sum $ subarray i j col)
+foldCol dp col = reverse $ foldl walk [] $ zip col [0..]
+    where walk :: [Int] -> (Int, Int) -> [Int]
+          walk acc target@(end, i) =
+              let path :: (Int, Int) -> Int
+                  path (start, j) = start + (sum $ subarray i j col)
+              in  (minimum $ map path $ zip dp [0..]):acc
 
 subarray :: Int -> Int -> [Int] -> [Int]
 subarray i j l
-    | i <= j = take (1 + j - i) $ drop i l
-    | j < i  = subarray j i l
+  | i <= j = take (1 + j - i) $ drop i l
+  | j < i  = subarray j i l
 
 main = do
     contents <- getContents
